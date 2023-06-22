0c7c24c151eded42e8963162c86c841fdcf4fa9c
Sepehr Liasi <sepehrghaseminejad@yahoo.com>
2023-06-22 22:24:48 +0330

    Comments added

--- TSP.py
+++ TSP.py
@@ -4,17 +4,19 @@ import math
 
 from matplotlib import pyplot as plt
 
-
+# Defining a point with two coordinates(x, y)
 class Point:
     def __init__(self, x: Union[int, float], y: Union[int, float]):
         self.x = x
         self.y = y
 
+    # Calculating the actual distance between two points
     def __sub__(self, other):
         return math.sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2)
 
-
+# Coding the main algorithm
 class TSP:
+    # Constructor of the TSP class, including the map cities with their corresponding coordinates as an input
     def __init__(
         self,
         points: Dict[Union[str, int], Point],
@@ -22,17 +24,23 @@ class TSP:
         alpha: int = 0.99,
         same_solution: int = 150000,
     ):
+        # Initializing TSP parameters
         self.points = points
         self.point_count = len(self.points)
         self.init_temp = init_temp
         self.alpha = alpha
         self.target_same_solution = same_solution
+
+        # The initial solution for the TSP is created by arranging the cities in the same order as they are listed in the dataset.
         self.solution = list(self.points.keys())
+
+        # Creating the matrix of distances between each of the two cities
         self.distances = {
             j: {i: self.points[i] - self.points[j] for i in points} for j in points
         }
         self.cost = self.calculate_distance(self.solution)
 
+    # Randomly selects one of the 4 functions to change the current TSP solution
     def get_new_solution(self):
         new_solution_generator = [
             self.inverse_path,
@@ -42,6 +50,7 @@ class TSP:
         ]
         return choice(new_solution_generator)()
 
+    # reverses a random subpath of the current TSP solution
     def inverse_path(self):
         start_index = randint(0, self.point_count - 2)
         end_index = randint(start_index + 1, self.point_count - 1)
@@ -51,6 +60,7 @@ class TSP:
         )
         return new_solution
 
+    # Swaps two randomly chosen points in the current TSP solution
     def swap_point(self) -> List[Union[int, str]]:
         first = randint(0, self.point_count - 2)
         second = randint(first + 1, self.point_count - 1)
@@ -61,6 +71,7 @@ class TSP:
         )
         return new_solution
 
+    # Changes the index of a randomly chosen point to another random position in the current TSP solution
     def change_point_position(self) -> List[Union[int, str]]:
         first = randint(0, self.point_count - 1)
         second = randint(0, self.point_count - 1)
@@ -69,6 +80,7 @@ class TSP:
         new_solution.insert(second, value)
         return new_solution
 
+    # Changes the index of the points of a randomly selected "subpath" to another random position in the current TSP solution
     def change_path_position(self) -> List[Union[int, str]]:
         start_index = randint(0, self.point_count - 2)
         end_index = randint(start_index + 1, self.point_count - 1)
@@ -79,6 +91,7 @@ class TSP:
         new_solution = new_solution[:break_index] + value + new_solution[break_index:]
         return new_solution
 
+    # Calculates the total distance of the input TSP solution (main cost)
     def calculate_distance(self, solution: List[Union[int, str]]) -> int:
         distance = 0
         for i in range(self.point_count - 1):
@@ -86,6 +99,7 @@ class TSP:
         distance += self.distances[solution[-1]][solution[0]]
         return distance
 
+    # Implementing the main algorithm (Simulated Annealing)
     def solve(self):
         cur_temp = self.init_temp
         same_solution = 0
@@ -115,6 +129,7 @@ class TSP:
                     same_solution += 1
             cur_temp *= self.alpha
 
+    # Displays the points and the TSP solution path on a graph
     def display_solution(self):
         x = [self.points[point_name].x for point_name in self.solution] + [
             self.points[self.solution[0]].x
--- main.py
+++ main.py
@@ -3,13 +3,20 @@ from TSP import Point, TSP
 
 
 if __name__ == "__main__":
+    # Reading the data from dataset
     data = pandas.read_csv("data.csv", header=None, names=["name", "x", "y"])
     data = data.set_index("name").apply(tuple, axis=1).to_dict()
     points = {
         name: Point(coordinates[0], coordinates[1])
         for name, coordinates in data.items()
     }
+
+    # Creating a TSP instance with arbitrary same_solution number
     tsp = TSP(points, same_solution=1500)
+
+    # Processing the TSP solution
     tsp.solve()
+
+    # Displaying the final solution
     print(f'Cost of final solution: {tsp.cost}')
     tsp.display_solution()

78434dfc85576b8c268787907f55798f90f118cf
Ali Ghaffari Sabet <aghs8055@gmail.com>
2023-06-22 20:50:48 +0330

    A minor bug in generating point objects fixed.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     data = pandas.read_csv("data.csv", header=None, names=["name", "x", "y"])
     data = data.set_index("name").apply(tuple, axis=1).to_dict()
     points = {
-        name: Point(coordinates[1], coordinates[0])
+        name: Point(coordinates[0], coordinates[1])
         for name, coordinates in data.items()
     }
     tsp = TSP(points, same_solution=1500)
